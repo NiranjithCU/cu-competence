07b7715624060af71df13fef2980d0559827ef7a
Niranjith Perumal <niranjithperumal@cunet.carleton.ca>
2023-03-22 21:50:39 -0400

    feat(auth): signout redirect

--- src/app/(main)/components/Nav.tsx
+++ src/app/(main)/components/Nav.tsx
@@ -59,7 +59,7 @@ export default function Nav() {
               </div>
               <button
                 className="ml-5 text-sm font-semibold leading-6 text-gray-900"
-                onClick={() => signOut()}
+                onClick={() => signOut({ callbackUrl: '/' })}
               >
                 Sign out
               </button>
@@ -118,7 +118,7 @@ export default function Nav() {
                     <Link
                       href="#"
                       className="-mx-3 block rounded-lg py-2 px-3 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-400/10"
-                      onClick={() => signOut()}
+                      onClick={() => signOut({ callbackUrl: '/' })}
                     >
                       Sign out
                     </Link>
